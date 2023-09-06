70bdba10a845b27457d8327bf8d6b352bd866750
Fianz <dctbagalyus@gmail.com>
2023-09-06 16:15:28 +0700

    update fiture

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import os
 
-def menu():
+low_alfa = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z']
+big_alfa = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Z']
+all_alfa = low_alfa + big_alfa
+
+
+
+"""fungsi menu"""
+def menu(title = "", title_sec = "", list_1 = "", list_2 = "", list_3 = "", list_4 = ""):
     os.system('cls')
-    print("="*50)
-    print("Tools Menghitung Osiloskop \n"
-          "Fitur\n"
-          "- Vdd\n"
-          "- frekuensi\n"
-          "- output ideal")
-    print("="*50)
+    print('='* 50)
+    print(title)
+    print('='* 50)
+    print(f'{title_sec}\n'
+          f'{list_1}\n'
+          f'{list_2}\n'
+          f'{list_3}\n'
+          f'{list_4}')
 
 def menu_luas_pp():
     # os.system('cls')
@@ -19,33 +27,51 @@ def menu_luas_pp():
     print("Untuk keluar masukan input string")
     print("="*50)
 
+"""fungsi persegi panjang"""
 def luas_persegi_panjang():
-    menu_luas_pp()
-    
-    panjang = int(input("Masukan panjang: "))
-    lebar = int(input("Masukan lebar: "))
-    luas = panjang * lebar
-    print(f"Luas Persegi panjang {luas}")
-
-def looping_funsion():
     loop = True
     while loop:
         answer = input("lanjutkan untuk perhitungan ini? ")
-        if answer == 'y' or 'Y':  
-            luas_persegi_panjang()
-        else:
-            loop = False
-        
+        if answer == 'y' or 'Y' and not all_alfa:
+            # print(type(answer))
+            # loop = False  
+            menu("LUAS PERSEGI", 
+                 "Gunakan perintah 'x' untuk keluar\nGunakan 'y' untuk melanjutkan",
+                 "Masukan Panjang persegi panjang",
+                 "Masukan Lebar Persegi panjang")
+            
+            try:
+                panjang = int(input("Masukan panjang: "))
+                luas = panjang * lebar
+                lebar = int(input("Masukan lebar: "))
+                print(f"Luas Persegi panjang {luas}")
+                
+            except ValueError:
+                print("Masukan input Angka please!!")
 
+        elif answer == 'x' or 'X' and not all_alfa:
+            # print(type(answer))
+            loop = False
+
+        elif all_alfa:
+            print("Masukan huruf sesuai perintah!!")
+    
+    
+        
+"""main loop"""
 def main():
     loop = True
 
-    menu()
     while loop:
+        menu("tools simple kalkulator",
+            "",
+            "osiloscop",
+            "luas persegi panjang",
+            "GLBB",)
         data_input = input("Pilih Metode Perhitungan: ")
         data_integer = int(data_input)
         if data_integer == 1:
-            looping_funsion()
+            luas_persegi_panjang()
             os.system('cls')
 
         if data_input == 'x':
